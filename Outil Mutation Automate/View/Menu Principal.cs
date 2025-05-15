74a52ff7505f5a529393346c94a8751fcf3b3b52
Calvin Ndiaye <calvin.ndiaye.pro@gmail.com>
2025-05-15 22:38:50 +0200

    Message d'erreur si tous les champs ne sont pas remplis.

--- Outil Mutation Automate/View/Menu Principal.cs
+++ Outil Mutation Automate/View/Menu Principal.cs
@@ -12,42 +12,48 @@ namespace Outil_Mutation_Automate
 
         private void BtnCalculer_Click(object sender, EventArgs e)
         {
-            // Récupérer le texte entré par l'utilisateur dans les TextBox.
-            // 'Hauteur', 'Frequence', 'Moyenne' et 'HauteurG'
-            string hauteurProduitTexte = hauteur.Text;
-            string frequencePickingTexte = frequence.Text;
-            string moyenneVentesTexte = moyenne.Text;
-            string hauteurCanalDesireTexte = hauteurG.Text;
-
-            // Tenter de convertir le texte récupéré en nombres (double pour les nombres décimaux, int pour les entiers).
-            // 'out double hauteurProduit' signifie que si la conversion réussit, la valeur sera stockée dans la variable 'hauteurProduit'.
-            if (double.TryParse(hauteurProduitTexte, out double hauteurProduit) &&
-                int.TryParse(frequencePickingTexte, out int frequencePicking) &&
-                double.TryParse(moyenneVentesTexte, out double moyenneVentes) &&
-                double.TryParse(hauteurCanalDesireTexte, out double hauteurCanalDesire))
+            if (!hauteur.Text.Equals("") && !frequence.Text.Equals("") && !moyenne.Text.Equals("") && !hauteurG.Text.Equals(""))
             {
-                // Logique de calcul en exemple à remplacer
-                double NBV = moyenneVentes / 25;
-                double HT = hauteurProduit * NBV;
-                double NbGoulotte = HT / hauteurCanalDesire;
-                double NBC = moyenneVentes / frequencePicking;
+                // Récupérer le texte entré par l'utilisateur dans les TextBox.
+                // 'Hauteur', 'Frequence', 'Moyenne' et 'HauteurG'
+                string hauteurProduitTexte = hauteur.Text;
+                string frequencePickingTexte = frequence.Text;
+                string moyenneVentesTexte = moyenne.Text;
+                string hauteurCanalDesireTexte = hauteurG.Text;
 
+                // Tenter de convertir le texte récupéré en nombres (double pour les nombres décimaux, int pour les entiers).
+                // 'out double hauteurProduit' signifie que si la conversion réussit, la valeur sera stockée dans la variable 'hauteurProduit'.
+                if (double.TryParse(hauteurProduitTexte, out double hauteurProduit) &&
+                    int.TryParse(frequencePickingTexte, out int frequencePicking) &&
+                    double.TryParse(moyenneVentesTexte, out double moyenneVentes) &&
+                    double.TryParse(hauteurCanalDesireTexte, out double hauteurCanalDesire))
+                {
+                    // Logique de calcul en exemple à remplacer
+                    double NBV = moyenneVentes / 25;
+                    double HT = hauteurProduit * NBV;
+                    double NbGoulotte = HT / hauteurCanalDesire;
+                    double NBC = moyenneVentes / frequencePicking;
 
-                // Afficher le résultat sous forme de texte
-                ligne1.Text = "• Nombre de boîtes vendues (par jour) :  " + NBV.ToString();
-                ligne2.Text = "• Hauteur totale nécessaire (par jour) : " + HT.ToString() + "mm";
-                ligne3.Text = "• Nombre de canaux de " + hauteurCanalDesire + "mm" + " nécessaire par jour : " + NbGoulotte;
-                ligne4.Text = "• Nombre de boîte par commande (en moyenne) : " + Math.Round(NBC, 1);
-
+                    // Afficher le résultat sous forme de texte
+                    ligne1.Text = "• Nombre de boîtes vendues (par jour) :  " + NBV.ToString();
+                    ligne2.Text = "• Hauteur totale nécessaire (par jour) : " + HT.ToString() + "mm";
+                    ligne3.Text = "• Nombre de canaux de " + hauteurCanalDesire + "mm" + " nécessaire par jour : " + NbGoulotte;
+                    ligne4.Text = "• Nombre de boîte par commande (en moyenne) : " + Math.Round(NBC, 1);
+                }
+                else
+                // Si une ou plusieurs conversions ont échoué (l'utilisateur n'a pas entré de nombres valides),
+                // afficher un message d'erreur 
+                {
+                    MessageBox.Show("Erreur : Veuillez saisir une valeur correcte.");
+                    return;
+                }
             }
             else
-            // Si une ou plusieurs conversions ont échoué (l'utilisateur n'a pas entré de nombres valides),
-            // afficher un message d'erreur dans le contrôle Label 'ResultatsTxt'.
-            {
-                ligne1.Text = "Erreur : Veuillez entrer des nombres valides.";
+            {   // Si l'un des champs est vide, afficher un message d'erreur.
+                MessageBox.Show("Erreur : Veuillez remplir tous les champs.");
+                return;
             }
         }
-
         // Fonction de détermination de la zone du produit correspondant
         public bool Zone(double frequence, int NBC, double NbGoulotte)
         {
