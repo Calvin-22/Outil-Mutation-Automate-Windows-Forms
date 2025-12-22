ca7353ec60e77e645301adfe6110a4d12af1da51
Calvin Ndiaye <calvin.ndiaye.pro@gmail.com>
2025-12-22 22:38:16 +0800

    Activation du mode SSL afin de crypter le tunnel des échanges entre client et serveur.

--- Outil Mutation Automate/Dal/Access.cs
+++ Outil Mutation Automate/Dal/Access.cs
@@ -17,7 +17,7 @@ namespace Outil_Mutation_Automate.Dal
         /// <summary>
         /// chaine de connexion à la bdd
         /// </summary>
-        private static readonly string connectionString = "server=localhost; user id=root; password=calvin22; database=bdd_mutation; SslMode=none";
+        private static readonly string connectionString = "server=localhost; user id=root; password=calvin22; database=bdd_mutation; SslMode=Required;";
         /// <summary>
         /// instance unique de la classe
         /// </summary>
